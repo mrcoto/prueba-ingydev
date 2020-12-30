1e690badaca94fc29735605d293012710bdad258
José Espinoza <espinozasalas.jose@gmail.com>
2020-12-30 11:00:03 -0300

    Added dotenv, express and jest

---
+++ productos_api/Readme.md
@@ -0,0 +1,16 @@
+# Backend Api - Prueba IngyDev
+
+## Requisitos
+
+- Node 12
+- Base de Datos PostgreSQL
+
+## Instrucciones
+
+- Ejecute `npm install`
+
+- Copie el archivo `.env.example` y nómbrelo `.env`, luego modifique las variables del `.env` de ser necesario.
+
+- Para ejecutar pruebas, corra el comando `npm test`
+
+- Para iniciar el servidor, ejecute `npm start`
---
+++ productos_api/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.SERVER_PORT
+
+const helmet = require('helmet')
+app.use(helmet())
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const compression = require('compression')
+app.use(compression())
+
+const cors = require('cors')
+app.use(cors())
+
+const morgan = require('morgan')
+app.use(morgan('tiny'))
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutandose en http://localhost:${port}`)
+})
