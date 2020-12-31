27a2db233f108d5b847d807047388839e6702ec2
José Espinoza <espinozasalas.jose@gmail.com>
2020-12-31 09:09:47 -0300

    Added initial routing

--- productosfrontend/src/app/app.module.ts
+++ productosfrontend/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ productosfrontend/src/app/app.routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'productos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
